168f6c2a58a7060be6f1f1a6adf32040e16cab23
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-07 11:51:48 -0400

    Add reset id

--- python/src/core/taskgraph.py
+++ python/src/core/taskgraph.py
@@ -1,6 +1,9 @@
 """TaskGraph module for Spider."""
 
-from core.task import Task, TaskId
+from copy import deepcopy
+from uuid import uuid4
+
+from core.task import Task, TaskId, TaskInputOutput
 
 
 class TaskGraph:
@@ -25,7 +28,7 @@ class TaskGraph:
         :param parents: The parent ids of the task. Must be already in the task graph.
         :param children: The children ids of the task. Must be already in the task graph.
         """
-        self.tasks[task.task_id] = task
+        self.tasks[task.task_id] = deepcopy(task)
         if parents:
             for parent in parents:
                 self.dependencies.append((parent, task.task_id))
@@ -54,3 +57,34 @@ class TaskGraph:
         :return: List of children tasks.
         """
         return [self.tasks[child] for (parent, child) in self.dependencies if parent == task_id]
+
+    def reset_task_ids(self) -> None:
+        """Resets task ids for all tasks."""
+        task_id_map: dict[TaskId, TaskId] = {}
+        for task_id in self.tasks:
+            task_id_map[task_id] = uuid4()
+
+        new_tasks: dict[TaskId, Task] = {}
+        for task_id in self.tasks:
+            new_task = self.tasks[task_id]
+            new_task.task_id = task_id_map[new_task.task_id]
+            for task_input in new_task.task_input:
+                if isinstance(task_input, TaskInputOutput):
+                    task_input.task_id = task_id_map[task_input.task_id]
+            new_tasks[new_task.task_id] = new_task
+        self.tasks = new_tasks
+
+        new_dependencies: list[tuple[TaskId, TaskId]] = []
+        for parent, child in self.dependencies:
+            new_dependencies.append((task_id_map[parent], task_id_map[child]))
+        self.dependencies = new_dependencies
+
+        new_input_tasks: set[TaskId] = set()
+        for task_id in self.input_tasks:
+            new_input_tasks.add(task_id_map[task_id])
+        self.input_tasks = new_input_tasks
+
+        new_output_tasks: set[TaskId] = set()
+        for task_id in self.output_tasks:
+            new_output_tasks.add(task_id_map[task_id])
+        self.output_tasks = new_output_tasks
